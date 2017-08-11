fab8751ba15e1b55abbd149c8f00530aa0714a0e
jl412 <liqiangjohnny@gmail.com>
2017-08-11 16:05:11 -0400

    test

--- assignment/app.js
+++ assignment/app.js
@@ -1,20 +1,19 @@
-// var app = require('../express');
-// var q = require('q');
-//
-// var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-// if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-//     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-//     var password = process.env.MLAB_PASSWORD_WEBDEV;
-//     connectionString = 'mongodb://' + username + ':' + password;
-//     connectionString += '@ds157282.mlab.com:57282/heroku_3z32f873'; // user yours
-//     console.log("Mongo running remotely");
-// }
-// // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
-// // above with your own URL given to you by mLab
-//
-// var mongoose = require("mongoose");
-// mongoose.connect(connectionString);
-// mongoose.Promise = q.Promise;
+var app = require('../../express');
+var q = require('q');
+
+
+var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds157282.mlab.com:57282/heroku_3z32f873'; // user yours
+    console.log("mongo running remotely");
+}
+
+var mongoose = require("mongoose");
+mongoose.connect(connectionString);
+mongoose.Promise = q.Promise;
 
 
 require('./services/user.service.server');
