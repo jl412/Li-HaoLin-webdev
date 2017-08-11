fa8cadf65fabd5d9ccb2b40c39be27160e96feac
jl412 <liqiangjohnny@gmail.com>
2017-08-11 15:07:07 -0400

    test

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-// require("./test/app.js");
-require("./assignment/app");
+require("./test/app.js");
+// require("./assignment/app");
 
 var port = process.env.PORT || 3000;
 
