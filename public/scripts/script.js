3ff85668e04c4e8f9c28829881a713832507fff4
jl412 <liqiangjohnny@gmail.com>
2017-08-21 06:00:52 -0400

    Homepage implementation

---
+++ public/scripts/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors: ['Home', 'Details'],
+        menu: '#menu'
+    });
+
+    $(window).load(function(){
+        $('.loading.initial').hide("fade", 1500);
+    });
+
+});
